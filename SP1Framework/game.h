436dc4f82fd682361f2bfd7816f815519717eb12
CHEONG KAI JIE <201429Z@mymail.nyp.edu.sg>
2021-08-24 14:55:42 +0800

    merge

--- SP1Framework/game.h
+++ SP1Framework/game.h
@@ -86,6 +86,8 @@ void gameplayKBHandler(const KEY_EVENT_RECORD& keyboardEvent);   // handles keyb
 void gameplayMouseHandler(const MOUSE_EVENT_RECORD& mouseEvent); // handles mouse events for gameplay 
 
 void loadlvl1();
+void lvl1TXT();
+void lvl1TXTclear();
 void loadlvl2();
 
 void gameOver();
